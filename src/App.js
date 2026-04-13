65db4fff8ebb430e2bf0013102a735395e6a1dbc
xinrandf <xinrandee@gmail.com>
2026-04-13 12:57:03 -0700

    work new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import Header from './features/header/Header'
+import TodoList from './features/todos/TodoList'
+import Footer from './features/footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <section>
-          <h1>Redux Fundamentals Example</h1>
-
-          <div className="navContent">
-            <div className="navLinks"></div>
-          </div>
-        </section>
-      </nav>
+      <nav> <section> <h1>Redux Fundamentals Example</h1> </section> </nav>
+      <main> 
+        <section className="medium-container">
+         <h2>Todos</h2>
+          <div className="todoapp"> 
+        <Header /> 
+        <TodoList />
+         <Footer />
+          </div> 
+          </section> 
+          </main>
       <section>
         <h2>Welcome to the Redux Fundamentals example app!</h2>
       </section>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-
+import { Provider } from 'react-redux'
+import store from './store' 
+import { fetchTodos } from './features/todos/todosSlice'
 import './api/server'
 
 ReactDOM.render(
